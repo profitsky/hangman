efd4b2f316a883ced20f4c5cf30871abc9bf4419
piotr <peter.zyskowski@gmail.com>
2021-02-11 01:20:16 +0100

    welcome -dotes- animation initialize

--- app/LightController.js
+++ app/LightController.js
@@ -4,17 +4,36 @@ export class Light
     {
         this.ledNumber = ledNumber;
         this.container = container;
+               
     };
 
     generateLed()
     {
-        for (let i = 0; i < this.ledNumber*1; i++)
-        {
-            let dot = document.createElement("div");
-            dot.classList.add("led-dot");
-            this.container.appendChild(dot);
-        }
+        let i = 0;
+        for (i = 0; i < (this.ledNumber*12); i++)
+        {     
+                  
+            let led = document.createElement("div");            
+            led.classList.add("led-dot");            
+            this.container.appendChild(led);
+            led.style.width = (100/(this.ledNumber*3))+ "%";            
+
+            if(i <= this.ledNumber*3  || i >= ((this.ledNumber*6)-1) && i <= (this.ledNumber*6) || i >= (this.ledNumber*9)-1)
+
+            {                      
+                let dotes = document.createElement("div");
+                dotes.classList.add("dot");
+                led.appendChild(dotes);               
+            };                    
+        };    
+    };
+
+    runAnimation()
+    {
+        
     }
+
+    
 
 
 };
